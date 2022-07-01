7f169b0e8b9bcae9ffdc2a1d412ca57c9c34c58c
Zufelde <374953@cognizant.com>
2022-07-01 16:20:32 +0200

    updated import from routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,13 @@
 import { Routes, RouterModule } from "@angular/router";
 
-import { ChatPartComponent } from "./app/chatPart";
-import { LoginPartComponent } from "./app/loginPart";
+import { ChatPartComponent } from "./app/chat-part.component";
+import { LoginPartComponent } from "./app/login-part.component";
 
 
 const APP_ROUTES: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'chat', component: ChatPartComponent },
-//  { path: 'login', component: loginPartComponent },  
+  { path: 'login', component: loginPartComponent },  
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
