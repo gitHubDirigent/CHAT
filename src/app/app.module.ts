4965eef5110ed8ca7c894505c926eb9bfb15364a
Zufelde <374953@cognizant.com>
2022-07-05 14:41:37 +0200

    auf dem Weg der Fehlersuche

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { routing } from "./app.routing";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 
+import { AppComponent } from './app.component';
 import { LoginPartComponent } from "./loginPart/LoginPart.component";
 import { ChatPartComponent } from "./chatPart/ChatPart.component";
+import { ChatService } from "./chatPart/ChatService.component";
 
 @NgModule({
-  declarations: [LoginPartComponent, ChatPartComponent],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: [LoginComponent]
+  declarations: [AppComponent, LoginPartComponent, ChatPartComponent],
+  imports: [BrowserModule, FormsModule, routing],
+  providers: [ChatService],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
+import './polyfills.ts';
+
 import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import { LoginPartComponent } from "./app/loginPart/LoginPart.component";
 import { environment } from "./environments/environment";
+import { AppModule } from './app/';
 
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic()
-  .bootstrapModule(LoginPartComponent)
+  .bootstrapModule(AppModule)
   .catch((err) => console.log(err));
